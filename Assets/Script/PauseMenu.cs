c0d3b4070560a147158ba19c62c0e9e0e168af97
unknown <LBI15XSupraveeN@TA11L151-18.opetus.kajak.fi>
2015-06-09 13:36:07 +0300

    Add Pause Menu

---
+++ Assets/Script/PauseMenu.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour
+{
+    public string levelSelect;
+
+    public bool isPaused;
+
+    public GameObject pauseMenuCanvas;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPaused)
+        {
+            pauseMenuCanvas.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            pauseMenuCanvas.SetActive(false);
+            Time.timeScale = 1f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+    }
+
+    public void Resume()
+    {
+        isPaused = false;
+    }
+
+    public void LevelSelect()
+    {
+        Application.LoadLevel(levelSelect);
+    }
+
+    public void Quit()
+    {
+        Application.LoadLevel(0);
+    }
+}
--- Assets/Script/UIManagerScript.cs
+++ Assets/Script/UIManagerScript.cs
@@ -3,8 +3,21 @@ using System.Collections;
 
 public class UIManagerScript : MonoBehaviour
 {
+    public string startLevel;
+    public string levelSelect;
+
     public void StartGame()
     {
         Application.LoadLevel(1);
+    }
+
+    public void LevelSelect()
+    {
+        Application.LoadLevel(levelSelect);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
