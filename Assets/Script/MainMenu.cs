b1455756262da3bb825ba3ec1b34ef7094b636e6
unknown <LBI15XSupraveeN@TA11L151-18.opetus.kajak.fi>
2015-06-09 10:29:11 +0300

    Add Simple Main Menu that have only a start button.

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Image startText;
+
+    // Use this for initialization
+    void Start()
+    {
+        startText = startText.GetComponent<Image>();
+    }
+
+
+
+    public void StartLevel()
+    {
+        Application.LoadLevel(1);
+    }
+
+}
